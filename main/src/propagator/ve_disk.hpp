bd212ed17c206e665f0813396cdf71e6fe5f9e77
nknk567 <noah.kubli@uzh.ch>
2025-09-25 13:57:14 +0200

    nb stat fill 0

--- main/src/propagator/ve_disk.hpp
+++ main/src/propagator/ve_disk.hpp
@@ -51,11 +51,13 @@ public:
 
     void computeForces(DomainType& domain, DataType& simData) override
     {
-        Base::computeForces(domain, simData);
-
         auto&        d     = simData.hydro;
         const size_t first = domain.startIndex();
         const size_t last  = domain.endIndex();
+
+        fill(get<"nb_it_stat">(d), first, last, 0);
+        Base::computeForces(domain, simData);
+
 
         disk::betaCooling(first, last, d, simData.star);
         timer.step("betaCooling");
