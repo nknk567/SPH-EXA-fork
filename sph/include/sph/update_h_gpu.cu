9a7646e43819df9331017b463c0ea12cb4fe3cb3
nknk567 <noah.kubli@uzh.ch>
2026-06-02 18:35:39 +0200

    fix update_h_gpu.cu

--- sph/include/sph/update_h_gpu.cu
+++ sph/include/sph/update_h_gpu.cu
@@ -80,6 +80,53 @@ bool updateSmoothingLengthGpu(const GroupView& grp, unsigned ng0, const unsigned
 template bool updateSmoothingLengthGpu(const GroupView& grp, unsigned ng0, const unsigned* nc, float* h, uint64_t*);
 template bool updateSmoothingLengthGpu(const GroupView& grp, unsigned ng0, const unsigned* nc, double* h, uint64_t*);
 
+// template<class Tc, class T, class KeyType>
+//__global__ void
+// updateSmoothingLengthIterativeGpuKernel(unsigned ng0, unsigned ngmax, const cstone::Box<Tc> box,
+//                                         const LocalIndex* grpStart, const LocalIndex* grpEnd, LocalIndex numGroups,
+//                                         const cstone::OctreeNsView<Tc, KeyType> tree, const Tc* x, const Tc* y,
+//                                         const Tc* z, T* h, unsigned* nc, LocalIndex* nidx, TreeNodeIndex* globalPool)
+//{
+//     unsigned laneIdx     = threadIdx.x & (GpuConfig::warpSize - 1);
+//     unsigned targetIdx   = 0;
+//     unsigned warpIdxGrid = (blockDim.x * blockIdx.x + threadIdx.x) >> GpuConfig::warpSizeLog2;
+//
+//     LocalIndex* neighborsWarp = nidx + ngmax * TravConfig::targetSize * warpIdxGrid;
+//
+//     while (true)
+//     {
+//         // first thread in warp grabs next target
+//         if (laneIdx == 0) { targetIdx = atomicAdd(&cstone::targetCounterGlob, 1); }
+//         targetIdx = cstone::shflSync(targetIdx, 0);
+//
+//         if (targetIdx >= numGroups) return;
+//
+//         LocalIndex bodyBegin = grpStart[targetIdx];
+//         LocalIndex bodyEnd   = grpEnd[targetIdx];
+//         LocalIndex i         = bodyBegin + laneIdx;
+//
+//         unsigned ncSph =
+//             1 + traverseNeighbors(bodyBegin, bodyEnd, x, y, z, h, tree, box, neighborsWarp, ngmax, globalPool)[0];
+//
+//         constexpr int ncMaxIteration = 9;
+//         for (int ncIt = 0; ncIt <= ncMaxIteration; ++ncIt)
+//         {
+//             bool repeat = (ncSph < ng0 / 4 || (ncSph - 1) > ngmax) && i < bodyEnd;
+//             if (!cstone::ballotSync(repeat)) { break; }
+//             if (repeat) { h[i] = updateH(ng0, ncSph, h[i]); }
+//             ncSph =
+//                 1 + traverseNeighbors(bodyBegin, bodyEnd, x, y, z, h, tree, box, neighborsWarp, ngmax,
+//                 globalPool)[0];
+//
+//             bool ncFail = (ncSph < ng0 / 4 || (ncSph - 1) > ngmax) && i < bodyEnd;
+//             if (ncIt == ncMaxIteration && ncFail) { ncSph = 1; }
+//         }
+//
+//         if (i >= bodyEnd) continue;
+//
+//         nc[i] = ncSph;
+//     }
+// }
 template<class Tc, class T, class KeyType>
 __global__ void
 updateSmoothingLengthIterativeGpuKernel(unsigned ng0, unsigned ngmax, const cstone::Box<Tc> box,
@@ -105,23 +152,27 @@ updateSmoothingLengthIterativeGpuKernel(unsigned ng0, unsigned ngmax, const csto
         LocalIndex bodyEnd   = grpEnd[targetIdx];
         LocalIndex i         = bodyBegin + laneIdx;
 
+        //        unsigned ncSph =
+        //            1 + traverseNeighbors(bodyBegin, bodyEnd, x, y, z, h, tree, box, neighborsWarp, ngmax,
+        //            globalPool)[0];
+        if (i >= bodyEnd) continue;
         unsigned ncSph =
-            1 + traverseNeighbors(bodyBegin, bodyEnd, x, y, z, h, tree, box, neighborsWarp, ngmax, globalPool)[0];
-
+            findNeighbors(i, x, y, z, h, tree, box, ngmax, neighborsWarp + laneIdx, TravConfig::targetSize);
         constexpr int ncMaxIteration = 9;
         for (int ncIt = 0; ncIt <= ncMaxIteration; ++ncIt)
         {
             bool repeat = (ncSph < ng0 / 4 || (ncSph - 1) > ngmax) && i < bodyEnd;
             if (!cstone::ballotSync(repeat)) { break; }
             if (repeat) { h[i] = updateH(ng0, ncSph, h[i]); }
-            ncSph =
-                1 + traverseNeighbors(bodyBegin, bodyEnd, x, y, z, h, tree, box, neighborsWarp, ngmax, globalPool)[0];
+            ncSph = 1 + findNeighbors(i, x, y, z, h, tree, box, ngmax, neighborsWarp + laneIdx, TravConfig::targetSize);
+            //                1 + traverseNeighbors(bodyBegin, bodyEnd, x, y, z, h, tree, box, neighborsWarp, ngmax,
+            //                globalPool)[0];
 
             bool ncFail = (ncSph < ng0 / 4 || (ncSph - 1) > ngmax) && i < bodyEnd;
             if (ncIt == ncMaxIteration && ncFail) { ncSph = 1; }
         }
 
-        if (i >= bodyEnd) continue;
+        //        if (i >= bodyEnd) continue;
 
         nc[i] = ncSph;
     }
