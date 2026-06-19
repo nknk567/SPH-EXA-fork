b6789fc655db5e3ff69d3f23b0c894d907ccef25
nknk567 <noah.kubli@uzh.ch>
2026-06-19 17:16:58 +0200

    implement asynchronous relaxation

--- physics/Disk/include/relaxation_gpu.hpp
+++ physics/Disk/include/relaxation_gpu.hpp
@@ -11,5 +11,5 @@ namespace disk
 {
 template<typename T, typename Th>
 void moveToLocalMinimumGPU(size_t first, size_t last, T* x, T* y, T* z, const Th* ax, const Th* ay, const Th* az,
-                           const Th* vx, const Th* vy, const Th* vz, const Th* dt, float minDt, const cstone::Box<T>& box);
+                           Th* vx, Th* vy, Th* vz, const Th* dt, float minDt, const cstone::Box<T>& box);
 }
