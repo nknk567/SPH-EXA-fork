ca7ea2c2301f061569c5353eb0f38009ed803093
nknk567 <noah.kubli@uzh.ch>
2026-06-19 17:13:56 +0200

    implement asynchronous relaxation

--- physics/Disk/include/relaxation.cu
+++ physics/Disk/include/relaxation.cu
@@ -7,7 +7,8 @@ namespace disk
 
 template<typename T, typename Th>
 __global__ void moveToLocalMinimumKernel(size_t first, size_t last, T* x, T* y, T* z, const Th* ax, const Th* ay,
-                                         const Th* az, Th* vx, Th* vy, Th* vz, const Th* dt, float minDt, const cstone::Box<T> box)
+                                         const Th* az, Th* vx, Th* vy, Th* vz, const Th* dt, float minDt,
+                                         const cstone::Box<T> box)
 {
     cstone::LocalIndex i = first + blockDim.x * blockIdx.x + threadIdx.x;
     if (i >= last) { return; }
@@ -27,20 +28,20 @@ __global__ void moveToLocalMinimumKernel(size_t first, size_t last, T* x, T* y, 
 
 template<typename T, typename Th>
 void moveToLocalMinimumGPU(size_t first, size_t last, T* x, T* y, T* z, const Th* ax, const Th* ay, const Th* az,
-                            Th* vx,  Th* vy,  Th* vz, const Th* dt, const cstone::Box<T>& box)
+                           Th* vx, Th* vy, Th* vz, const Th* dt, const cstone::Box<T>& box)
 {
     cstone::LocalIndex numParticles = last - first;
     unsigned           numThreads   = 256;
     unsigned           numBlocks    = (numParticles + numThreads - 1) / numThreads;
 
-    moveToLocalMinimumKernel<<<numBlocks, numThreads>>>(first, last, x, y, z, ax, ay, az, dt, box);
+    moveToLocalMinimumKernel<<<numBlocks, numThreads>>>(first, last, x, y, z, ax, ay, az, vx, vy, vz, dt, box);
 
     checkGpuErrors(cudaDeviceSynchronize());
 }
 
 #define MOVE_TO_LOCAL_MINIMUM_GPU(T, Th)                                                                               \
-    template void moveToLocalMinimumGPU(size_t, size_t, T*, T*, T*, const Th*, const Th*, const Th*,  Th*,        \
-                                         Th*,  Th*, const Th*, const cstone::Box<T>&);
+    template void moveToLocalMinimumGPU(size_t, size_t, T*, T*, T*, const Th*, const Th*, const Th*, Th*, Th*, Th*,    \
+                                        const Th*, const cstone::Box<T>&);
 
 MOVE_TO_LOCAL_MINIMUM_GPU(double, float);
 
