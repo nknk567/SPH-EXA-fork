7d481f34227be1bee3d66ad2d7fe1593bb6a3583
nknk567 <noah.kubli@uzh.ch>
2026-07-26 13:00:47 +0200

    increase ncmax

--- sph/include/sph/neighborhood_gpu.hpp
+++ sph/include/sph/neighborhood_gpu.hpp
@@ -63,7 +63,7 @@ struct DeviceNeighborhoodData::Impl
             neighborhood.emplace<0>();
             subgroupNeighborhood.reset();
 
-            const unsigned ncmax = d.ngmax * 6;
+            const unsigned ncmax = d.ngmax * 5;
 
             std::variant<cstone::ijloop::GpuAlwaysTraverseNeighborhoodBuilder, ClusteredNeighborhoodBuilder<false>,
                          ClusteredNeighborhoodBuilder<true>>
